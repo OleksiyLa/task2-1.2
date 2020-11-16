f52102db20930afed398798ac05a7e9c821b61fb
Oleksiy Lazarenko <alexusunit@gmail.com>
2020-11-16 07:46:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let a = prompt("type a number");
+let b = prompt("type a number");
+let c = prompt("type a number");
+
+let sum = +a + +b + +c;
+alert("sum of all variables is" + sum);
+if (a % 2 == 0) {
+    alert("variable 'a' is an even number");
+}
+if (b % 2 == 0) {
+    alert("variable 'b' is an even number");
+}
+if (c % 2 == 0) {
+    alert("variable 'c' is an even number");
+}
